63902426ff7d4cc3af423c54a2b0bc722d1c623f
Filip Chodura <choduraf@gmail.com>
2022-07-28 15:51:37 +0200

    Finnaly Mario can DIE (event handleres fix)

--- scenes/GameOver.js
+++ scenes/GameOver.js
@@ -8,17 +8,32 @@ export default class GameOver extends Phaser.Scene {
         let hud = this.scene.get('UIScene');
         //this.cameras.main.setBackgroundColor('#000');
         //Not ideal solution but works
+        this.sys.events.once('shutdown', this.shutdown, this);
+
 
         this.events.emit('gameOver'); 
 
-        hud.events.on('menu', function(){
-            this.scene.stop('game').stop('GameOver').start('menu')
-        }, this)
-        hud.events.on('gameOn', function(){
-            console.log('warum ')
-            this.scene.stop('GameOver').start('game')
-        }, this)
+        hud.events.once('menu', this.callMenu, this)
+        hud.events.once('gameOn', this.callGame, this)
 
 
     }
+
+    callMenu(){
+        this.scene.start('menu').stop('GameOver')
+        //this.scene.restart('GameOver')
+    }
+
+    callGame(){
+        console.log('warum ')
+        this.scene.start('game').stop('GameOver')
+        //this.scene.stop('GameOver')
+    }
+
+    shutdown(){
+        console.log("Konec mě")
+        this.events.off('menu', this.callMenu)
+        this.events.off('gameOn', this.callGame)
+    }
+
 }
--- scenes/Hud.js
+++ scenes/Hud.js
@@ -3,84 +3,123 @@ export default class Hud extends Phaser.Scene{
         super({ key: 'UIScene', active: true });
         this.score = 0;
         this.coins = 0;
-        this.timing = 0;
-        this.lives = 2;
+        this.timing = 400;
+        this.lives = 3;
     }
 
     create(){
+        this.sys.events.once('shutdown', this.shutdown, this);
         //HUD
-        let scoreText = this.add.text(20, 20, 'MARIO \n0')
-        let coinsText = this.add.text(window.innerWidth/3, 20, ' \nCOINS x 0')
+        this.scoreText = this.add.text(20, 20, 'MARIO \n0')
+        console.log('MAR')
+        this.coinsText = this.add.text(window.innerWidth/3, 20, ' \nCOINS x 0')
         this.add.text(window.innerWidth/1.5, 20, 'WORLD \n1-1')
-        let timeText = this.add.text(window.innerWidth-50, 20, 'TIME \n0')
+        this.timeText = this.add.text(window.innerWidth-50, 20, 'TIME \n0')
 
-        let gameOverText = this.add.text(window.innerWidth/2, 180, 'GAME OVER') 
-        gameOverText.visible = false;
-        let livesText = this.add.text(window.innerWidth/2, 180, 'LIVES x '+ this.lives) 
-        livesText.visible = false;
+        this.gameOverText = this.add.text(window.innerWidth/2, 180, 'GAME OVER') 
+        this.gameOverText.visible = false;
 
-        let Game = this.scene.get('game');
-        Game.events.on('addScore', function (){
-            this.score += 10;
-            scoreText.setText('MARIO \n' + this.score);
-        }, this)
-        Game.events.on('addCoin', function (){
-            this.coins += 1;
-            coinsText.setText('\nCOINS x ' + this.coins);
-        }, this)
+        this.livesText = this.add.text(window.innerWidth/2, 180, 'LIVES x '+ this.lives) 
+        this.livesText.visible = false;
+
+        const Game = this.scene.get('game');
+        Game.events.once('addScore', this.addScore, this)
+        Game.events.once('addCoin', this.addCoin , this)
         
-        let timer;
-        Game.events.on('startTimer', function(){
-            timer = this.time.addEvent({
-                delay: 1000,
-                callback: function(){
-                    this.timing ++;
-                    timeText.setText('TIME \n' + this.timing)
-                    //console.log(this.timing)
-                },
-                callbackScope: this,
-                loop: true
-            });
-        }, this)
+        this.timeToDie;
+        Game.events.once('startTimer', this.timeOut, this)
         
         let GameOver = this.scene.get('GameOver');
-        GameOver.events.on('gameOver', function(){
-
-            this.lives = this.lives-1;
-            this.score = 0;
-            this.coins = 0;
-            
-            if(this.lives == 0){
-                gameOverText.visible = true
-                livesText.visible = false;
-                this.score = 0;
-                this.coins = 0;
-
-            }else{
-                livesText.setText('LIVES x '+ this.lives)
-                livesText.visible = true;
-                gameOverText.visible = false
-                timer.paused = true;
-            }
-            this.time.addEvent({
-                delay: 3000,
-                callback: () => {
-
-                    livesText.visible = false;
-                    gameOverText.visible = false;
-                    
-                    if(this.lives == 0){
-                        this.lives = 3;
-                        timer = null;
-                        this.timing = 0;
-                        this.events.emit('menu')
-                    }else{
-                        this.events.emit('gameOn')
-                    }
-                }
-            })
-        }, this)
+        GameOver.events.once('gameOver', this.gameOver, this)
 
     }
 
+    addScore(){
+        this.score += 10;
+        this.scoreText.setText('MARIO \n' + this.score);
+    }
+
+    addCoin(){
+        this.coins += 1;
+        this.coinsText.setText('\nCOINS x ' + this.coins);
+    }
+
+    timeOut(){
+        this.timeToDie = this.time.addEvent({
+            delay: 1000,
+            callback: function(){
+                this.timing --;
+                this.timeText.setText('TIME \n' + this.timing)
+
+                if(this.timing == 0){
+                    this.lives == 0;
+                    this.gameOver();
+                }
+
+                //console.log(this.timing)
+            },
+            callbackScope: this,
+            loop: true
+        });
+    }
+
+    gameOver() {
+        this.lives = this.lives-1;
+        console.log(this.lives)
+        this.score = 0;
+        this.coins = 0;
+        
+        if(this.lives == 0){
+            this.gameOverText.visible = true
+            this.livesText.visible = false;
+            this.score = 0;
+            this.coins = 0;
+            this.timing = 400;
+
+        }else{
+            this.livesText.setText('LIVES x '+ this.lives)
+            this.livesText.visible = true;
+            this.gameOverText.visible = false
+            this.timeToDie.paused = true;
+            this.timing = 400;
+        }
+        this.time.addEvent({
+            delay: 3000,
+            callback: () => {
+
+                this.livesText.visible = false;
+                this.gameOverText.visible = false;
+                
+                if(this.lives == 0){
+                    this.lives = 3;
+
+                    this.timing = 0;
+                    this.scene.restart('UIScene')
+                    this.events.emit('menu')
+                    
+                }else{
+                    this.scene.restart('UIScene')
+                    this.events.emit('gameOn')
+                    
+                }
+
+                //this.scene.restart()
+            },
+            loop: false
+        })
+    }
+    
+    shutdown(){
+        console.log('Jsem zde')
+
+        this.events.off('addScore', this.addScore);
+        this.events.off('addCoin', this.addCoin);
+        this.events.off('startTimer', this.timeOut);
+        this.events.off('gameOver', this.gameOver);
+
+        this.time.removeEvent(this.timeToDie);
+
+        this.registry.destroy();
+        //this.events.destroy();
+    }
 }
