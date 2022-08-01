313472387c60d3aeac9191fd1a3663b60e7d9bc3
Filip Chodura <choduraf@gmail.com>
2022-08-01 21:20:50 +0200

    Minor bug fixes

--- gameObjects/BumpBlock.js
+++ gameObjects/BumpBlock.js
@@ -15,7 +15,7 @@ export default class QuestionBlock {
             this.questionBlocks.create(question.x, question.y + 152, 'questionBlock')
                 .setOrigin(0)
                 .setDepth(2)
-                .body.offset.y = 2;
+                .body.offset.y = 2; 
                 //Fix for uncollectible blocks, not prettiest solution but works
         });
         let idVar = 0;
